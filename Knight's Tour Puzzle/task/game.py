a6b135c099d9c04154e256ac2c2bbf23bc65b8b6
Alejandro RM <alejandror45@gmail.com>
2023-08-05 13:47:47 -0600

    Stage 3/6: Where to next?   The knight is ready to move. Your program needs to check where your knight is allowed to go.

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -1,35 +1,106 @@
-def validate(coords):
+def validate_coords(coords, dimensions):
     if len(coords) != 2:
         return False
 
-    for n in coords:
-        if not n.isdigit():
+    for i in range(0, 2):
+        if not coords[i].isdigit():
             return False
-        elif int(n) < 1 or int(n) > 8:
+        elif int(coords[i]) < 1 or int(coords[i]) > dimensions[i]:
             return False
 
     return True
 
 
+def validate_dimensions(dimensions):
+    if len(dimensions) != 2:
+        return False
+
+    for n in dimensions:
+        if not n.isdigit():
+            return False
+        elif int(n) < 1:
+            return False
+
+    return True
+
+
+class KnightTour:
+    def __init__(self, rows, columns):
+        self.columns = columns
+        self.rows = rows
+        self.current_column = None
+        self.current_row = None
+        self.cell_size = len(str(self.columns * self.rows))
+        self.border_length = self.columns * (self.cell_size + 1) + 3
+        self.board = [
+            ["_" * self.cell_size for _ in range(0, self.columns)]
+            for _ in range(0, self.rows)
+        ]
+
+    def set_position(self, row,  column, symbol="X"):
+        if symbol == "X":
+            self.current_column = column
+            self.current_row = row
+
+        self.board[row - 1][column - 1] = symbol.rjust(self.cell_size)
+
+    def set_possible_moves(self):
+        if self.current_column - 2 >= 1:
+            if self.current_row + 1 <= self.rows:
+                self.set_position(self.current_row + 1, self.current_column - 2, "O")
+            if self.current_row - 1 >= 1:
+                self.set_position(self.current_row - 1, self.current_column - 2, "O")
+
+        if self.current_column + 2 <= self.columns:
+            if self.current_row + 1 <= self.rows:
+                self.set_position(self.current_row + 1, self.current_column + 2, "O")
+            if self.current_row - 1 >= 1:
+                self.set_position(self.current_row - 1, self.current_column + 2, "O")
+
+        if self.current_row - 2 >= 1:
+            if self.current_column + 1 <= self.columns:
+                self.set_position(self.current_row - 2, self.current_column + 1, "O")
+            if self.current_column - 1 >= 1:
+                self.set_position(self.current_row - 2, self.current_column - 1, "O")
+
+        if self.current_row + 2 <= self.rows:
+            if self.current_column + 1 <= self.columns:
+                self.set_position(self.current_row + 2, self.current_column + 1, "O")
+            if self.current_column - 1 >= 1:
+                self.set_position(self.current_row + 2, self.current_column - 1, "O")
+
+    def print_board(self):
+        print("-" * self.border_length)
+        for i in range(len(self.board), 0, -1):
+            print(f"{i}| {' '.join(self.board[i - 1])} |")
+        print("-" * self.border_length)
+        print(f"   {' '.join([str(x).rjust(self.cell_size) for x in range(1, self.columns + 1)])} ")
+
+
 if __name__ == "__main__":
-    coords = input("Enter the knight's starting position:")
-    coords = coords.split(" ")
+    dimensions = None
+    coords = None
 
-    if not validate(coords):
-        print("Invalid dimensions!")
-        exit()
+    while True:
+        dimensions = input("Enter your board dimensions:")
+        dimensions = dimensions.split(" ")
+        if not validate_dimensions(dimensions):
+            print("Invalid dimensions!")
+        else:
+            dimensions = list(map(int, dimensions))
+            break
 
-    coords = list(map(int, coords))
-    board = [["_" for _ in range(0, 8)] for _ in range(0, 8)]
-    board[coords[1] - 1][coords[0] - 1] = "X"
-    print(" -------------------")
-    print("8| " + " ".join(board[7]) + " |")
-    print("7| " + " ".join(board[6]) + " |")
-    print("6| " + " ".join(board[5]) + " |")
-    print("5| " + " ".join(board[4]) + " |")
-    print("4| " + " ".join(board[3]) + " |")
-    print("3| " + " ".join(board[2]) + " |")
-    print("2| " + " ".join(board[1]) + " |")
-    print("1| " + " ".join(board[0]) + " |")
-    print(" -------------------")
-    print("   1 2 3 4 5 6 7 8 ")
+    puzzle = KnightTour(dimensions[1], dimensions[0])
+
+    while True:
+        coords = input("Enter the knight's starting position:")
+        coords = coords.split(" ")
+        if not validate_coords(coords, dimensions):
+            print("Invalid position!")
+        else:
+            coords = list(map(int, coords))
+            break
+
+    puzzle.set_position(coords[1], coords[0])
+    puzzle.set_possible_moves()
+    puzzle.print_board()
