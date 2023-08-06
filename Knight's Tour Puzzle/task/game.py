eff4ffd999985ff0a18f2b0446ff6a6ff9b7f87a
Alejandro RM <alejandror45@gmail.com>
2023-08-06 00:31:36 -0600

    Stage 6/6: Give me an answer   This puzzle is too hard! Let's make our program find a solution for us.

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -1,16 +1,3 @@
-def validate_coords(coords, dimensions):
-    if len(coords) != 2:
-        return False
-
-    for i in range(0, 2):
-        if not coords[i].isdigit():
-            return False
-        elif int(coords[i]) < 1 or int(coords[i]) > dimensions[i]:
-            return False
-
-    return True
-
-
 def validate_dimensions(dimensions):
     if len(dimensions) != 2:
         return False
@@ -25,49 +12,81 @@ def validate_dimensions(dimensions):
 
 
 class KnightTour:
-    def __init__(self, rows, columns):
+    def __init__(self, columns, rows):
+        self.moves = 0
         self.columns = columns
         self.rows = rows
         self.current_column = None
         self.current_row = None
+        self.total_cells = columns * rows
         self.cell_size = len(str(self.columns * self.rows))
         self.border_length = self.columns * (self.cell_size + 1) + 3
         self.board = [
             ["_" * self.cell_size for _ in range(0, self.columns)]
             for _ in range(0, self.rows)
         ]
+        self.move_col = [-2, -2, 2, 2, 1, -1, 1, -1]
+        self.move_row = [1, -1, 1, -1, -2, -2, 2, 2]
 
-    def set_position(self, row,  column, symbol="X"):
+    def reset(self):
+        self.moves = 0
+        self.board = [
+            ["_" * self.cell_size for _ in range(0, self.columns)]
+            for _ in range(0, self.rows)
+        ]
+
+    def set_position(self, column, row,  symbol="X"):
         if symbol == "X":
+            if self.current_column:
+                self.board[self.current_row-1][self.current_column-1] = "*".rjust(self.cell_size)
+
             self.current_column = column
             self.current_row = row
+            self.moves += 1
+
+            for i in range(self.rows):
+                for j in range(self.columns):
+                    if self.board[i][j].strip().isdigit():
+                        self.board[i][j] = "_" * self.cell_size
 
         self.board[row - 1][column - 1] = symbol.rjust(self.cell_size)
 
     def set_possible_moves(self):
-        if self.current_column - 2 >= 1:
-            if self.current_row + 1 <= self.rows:
-                self.set_position(self.current_row + 1, self.current_column - 2, "O")
-            if self.current_row - 1 >= 1:
-                self.set_position(self.current_row - 1, self.current_column - 2, "O")
+        def explore_moves(col, row):
+            if col < 1 or col > self.columns or row < 1 or row > self.rows or self.board[row-1][col-1].strip() == "*":
+                return
 
-        if self.current_column + 2 <= self.columns:
-            if self.current_row + 1 <= self.rows:
-                self.set_position(self.current_row + 1, self.current_column + 2, "O")
-            if self.current_row - 1 >= 1:
-                self.set_position(self.current_row - 1, self.current_column + 2, "O")
+            self.set_position(col, row, str(self.count_possible_move(col, row)))
 
-        if self.current_row - 2 >= 1:
-            if self.current_column + 1 <= self.columns:
-                self.set_position(self.current_row - 2, self.current_column + 1, "O")
-            if self.current_column - 1 >= 1:
-                self.set_position(self.current_row - 2, self.current_column - 1, "O")
+        explore_moves(self.current_column - 2, self.current_row + 1)
+        explore_moves(self.current_column - 2, self.current_row - 1)
+        explore_moves(self.current_column + 2, self.current_row + 1)
+        explore_moves(self.current_column + 2, self.current_row - 1)
+        explore_moves(self.current_column + 1, self.current_row - 2)
+        explore_moves(self.current_column - 1, self.current_row - 2)
+        explore_moves(self.current_column + 1, self.current_row + 2)
+        explore_moves(self.current_column - 1, self.current_row + 2)
 
-        if self.current_row + 2 <= self.rows:
-            if self.current_column + 1 <= self.columns:
-                self.set_position(self.current_row + 2, self.current_column + 1, "O")
-            if self.current_column - 1 >= 1:
-                self.set_position(self.current_row + 2, self.current_column - 1, "O")
+    def count_possible_move(self, column, row):
+        def count_moves_recursive(col, rw):
+            if (col < 1 or col > self.columns or rw < 1 or rw > self.rows or
+                    self.board[rw-1][col-1].strip() == "X" or
+                    self.board[rw-1][col-1].strip() == "*"):
+                return 0
+            else:
+                return 1
+
+        moves = 0
+        moves += count_moves_recursive(column - 2, row + 1)
+        moves += count_moves_recursive(column - 2, row - 1)
+        moves += count_moves_recursive(column + 2, row + 1)
+        moves += count_moves_recursive(column + 2, row - 1)
+        moves += count_moves_recursive(column + 1, row - 2)
+        moves += count_moves_recursive(column - 1, row - 2)
+        moves += count_moves_recursive(column + 1, row + 2)
+        moves += count_moves_recursive(column - 1, row + 2)
+
+        return moves
 
     def print_board(self):
         print("-" * self.border_length)
@@ -75,6 +94,53 @@ class KnightTour:
             print(f"{i}| {' '.join(self.board[i - 1])} |")
         print("-" * self.border_length)
         print(f"   {' '.join([str(x).rjust(self.cell_size) for x in range(1, self.columns + 1)])} ")
+
+    def validate_coords(self, coords):
+        dimensions = [self.columns, self.rows]
+        if len(coords) != 2:
+            return False
+
+        for i in range(0, 2):
+            if not str(coords[i]).isdigit():
+                return False
+            elif coords[i] < 1 or coords[i] > dimensions[i]:
+                return False
+
+        if (self.board[coords[1]-1][coords[0]-1].strip() == "X" or
+                self.board[coords[1]-1][coords[0]-1].strip() == "*"):
+            return False
+
+        return True
+
+    def has_more_moves(self):
+        return self.count_possible_move(self.current_column, self.current_row) > 0
+    
+    def is_l_move(self, coords):
+        col_diff = abs(coords[0] - self.current_column)
+        row_diff = abs(coords[1] - self.current_row)
+        return (col_diff == 1 and row_diff == 2) or (col_diff == 2 and row_diff == 1)
+
+    def has_won(self):
+        return self.moves == self.total_cells
+
+    def solution(self, row, col, move_count):
+        if move_count == self.columns * self.rows:
+            return True
+        for i in range(8):
+            next_row = row + self.move_row[i]
+            next_col = col + self.move_col[i]
+            if (0 <= next_col < self.columns and 0 <= next_row < self.rows and
+                    not self.board[next_row][next_col].strip().isdigit()):
+                self.board[next_row][next_col] = str(move_count + 1).rjust(self.cell_size)
+                if self.solution(next_row, next_col, move_count + 1):
+                    return True
+                self.board[next_row][next_col] = "_" * self.cell_size
+
+        return False
+
+    def find_solution(self):
+        self.board[self.current_row-1][self.current_column-1] = "1".rjust(self.cell_size)
+        return self.solution(self.current_row-1, self.current_column-1, 1)
 
 
 if __name__ == "__main__":
@@ -90,17 +156,63 @@ if __name__ == "__main__":
             dimensions = list(map(int, dimensions))
             break
 
-    puzzle = KnightTour(dimensions[1], dimensions[0])
+    puzzle = KnightTour(dimensions[0], dimensions[1])
 
     while True:
-        coords = input("Enter the knight's starting position:")
-        coords = coords.split(" ")
-        if not validate_coords(coords, dimensions):
-            print("Invalid position!")
-        else:
+        try:
+            coords = input("Enter the knight's starting position:")
+            coords = coords.split(" ")
             coords = list(map(int, coords))
+            if not puzzle.validate_coords(coords):
+                raise Exception
+            else:
+                break
+        except:
+            print("Invalid position!")
+
+    puzzle.set_position(coords[0], coords[1])
+
+    while True:
+        want_try = input("Do you want to try the puzzle? (y/n):")
+        if want_try == "y":
+            if puzzle.find_solution():
+                puzzle.reset()
+                puzzle.set_position(coords[0], coords[1])
+            else:
+                print("No solution exists!")
+                break
+
+            while puzzle.has_more_moves():
+                while True:
+                    try:
+                        coords = input("Enter your next move:")
+                        coords = coords.split(" ")
+                        coords = list(map(int, coords))
+                        if not puzzle.validate_coords(coords) or not puzzle.is_l_move(coords):
+                            print("Invalid move!", end="")
+                        else:
+                            break
+                    except:
+                        print("Invalid move!")
+
+                puzzle.set_position(coords[0], coords[1])
+                puzzle.set_possible_moves()
+                puzzle.print_board()
+
+            if puzzle.has_won():
+                print("What a great tour! Congratulations!")
+            else:
+                print("No more possible moves!")
+                print(f"Your knight visited {puzzle.moves} squares!")
             break
 
-    puzzle.set_position(coords[1], coords[0])
-    puzzle.set_possible_moves()
-    puzzle.print_board()
+        elif want_try == "n":
+            if puzzle.find_solution():
+                print("Here's the solution!")
+                puzzle.print_board()
+            else:
+                print("No solution exists!")
+            break
+
+        else:
+            print("Invalid input!")
